ef0b16bfe38a04fb81d369433eae4cd5d4720b37
oliwia2800 <s186710@student.pg.edu.pl>
2024-11-21 19:35:24 +0100

    Podstawowe użycie pakietu naniar.

--- Braki_w_danych.R
+++ Braki_w_danych.R
@@ -1,2 +1,11 @@
 Data <- read.csv("application_data_new.csv")
-View(Data)
+
+install.packages("naniar")
+library(naniar)
+
+NA_count <- n_miss(Data)
+complete_values <- n_complete(Data)
+NA_proportion <- prop_miss(Data)
+NA_percentage <- pct_miss(Data)
+NA_summary <- miss_var_summary(Data)
+NA_summary_case <- miss_case_table(Data)
